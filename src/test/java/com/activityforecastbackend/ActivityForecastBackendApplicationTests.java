7d0389e5da30e03d693e35ca82edf80e80e46124
Hwang-sebin <sbsimon0701@gmail.com>
2025-10-27 15:24:15 +0900

    Initial commit

---
+++ src/test/java/com/activityforecastbackend/ActivityForecastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activityforecastbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityForecastBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
